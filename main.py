b3e54a795c3a56a84871dd12f17ca50b4b4272da
narasimhachakravarti <narasimha.chakravarti@agiliztech.com>
2021-11-21 06:36:20 +0530

    Main and utils file changes

--- main.py
+++ main.py
@@ -1,154 +1,99 @@
-'''Train CIFAR10 with PyTorch.'''
-import torch
+from models.resnet import *
+from utils import *
+
+
+from torch.optim.lr_scheduler import StepLR, OneCycleLR
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
-import torch.backends.cudnn as cudnn
-
-import torchvision
-import torchvision.transforms as transforms
-
-import os
-import argparse
-
-from models import *
-from utils import progress_bar
 
 
-parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
-parser.add_argument('--resume', '-r', action='store_true',
-                    help='resume from checkpoint')
-args = parser.parse_args()
+PAD = 4
+EPOCHS = 20
+LR = 0.001
+MOMENTUM = 0.8
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-best_acc = 0  # best test accuracy
-start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
-# Data
-print('==> Preparing data..')
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+classes = ["airplane", "automobile", "bird", "cat", "deer",
+           "dog", "frog", "horse", "ship", "truck"]
 
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+print(f"----------Compute image mean & std----------")
+trainloader = get_train_loader(transform=None)
+mean, std = get_stats(trainloader)
 
-trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
+denorm = UnNormalize(mean, std)
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer',
-           'dog', 'frog', 'horse', 'ship', 'truck')
+print(f"----------Normailizing Images----------")
+train_transform = get_train_transform(mean, std)
+test_transform = get_test_transform(mean, std)
 
-# Model
-print('==> Building model..')
-# net = VGG('VGG19')
-# net = ResNet18()
-# net = PreActResNet18()
-# net = GoogLeNet()
-# net = DenseNet121()
-# net = ResNeXt29_2x64d()
-# net = MobileNet()
-# net = MobileNetV2()
-# net = DPN92()
-# net = ShuffleNetG2()
-# net = SENet18()
-# net = ShuffleNetV2(1)
-# net = EfficientNetB0()
-# net = RegNetX_200MF()
-net = SimpleDLA()
-net = net.to(device)
-if device == 'cuda':
-    net = torch.nn.DataParallel(net)
-    cudnn.benchmark = True
+print(f"----------Load and Transform Images----------")
+trainloader = get_train_loader(transform=train_transform)
+testloader = get_test_loader(transform=test_transform)
 
-if args.resume:
-    # Load checkpoint.
-    print('==> Resuming from checkpoint..')
-    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-    checkpoint = torch.load('./checkpoint/ckpt.pth')
-    net.load_state_dict(checkpoint['net'])
-    best_acc = checkpoint['acc']
-    start_epoch = checkpoint['epoch']
+
+device = get_device()
+print(f"----------Device type - {device}----------")
+
+
+print(f"----------Model Summary----------")
+model = ResNet18().to(device)
+get_summary(model, device)
+
+
+model = ResNet18().to(device)
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=args.lr,
-                      momentum=0.9, weight_decay=5e-4)
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+optimizer = optim.SGD(model.parameters(), lr=LR, momentum=MOMENTUM)
+scheduler = OneCycleLR(optimizer, max_lr=0.05,
+                       epochs=EPOCHS, steps_per_epoch=len(trainloader))
+
+print(f"----------Training Model----------")
+results = train_model(model, criterion, device, trainloader,
+                      testloader, optimizer, scheduler, EPOCHS)
+
+print(f"----------Loss & Accuracy Plots----------")
+make_plot(results)
 
 
-# Training
-def train(epoch):
-    print('\nEpoch: %d' % epoch)
-    net.train()
-    train_loss = 0
-    correct = 0
-    total = 0
-    for batch_idx, (inputs, targets) in enumerate(trainloader):
-        inputs, targets = inputs.to(device), targets.to(device)
-        optimizer.zero_grad()
-        outputs = net(inputs)
-        loss = criterion(outputs, targets)
-        loss.backward()
-        optimizer.step()
-
-        train_loss += loss.item()
-        _, predicted = outputs.max(1)
-        total += targets.size(0)
-        correct += predicted.eq(targets).sum().item()
-
-        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                     % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+max_images = 128
+test_images = [x[0] for x in testloader.dataset]
+test_images = torch.stack(test_images[:max_images])
+test_targets = torch.tensor(testloader.dataset.targets[:max_images]).to(device)
+print(f"----------Inference on {max_images} test images----------")
 
 
-def test(epoch):
-    global best_acc
-    net.eval()
-    test_loss = 0
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for batch_idx, (inputs, targets) in enumerate(testloader):
-            inputs, targets = inputs.to(device), targets.to(device)
-            outputs = net(inputs)
-            loss = criterion(outputs, targets)
+test_predictions = model(test_images.to(device))
 
-            test_loss += loss.item()
-            _, predicted = outputs.max(1)
-            total += targets.size(0)
-            correct += predicted.eq(targets).sum().item()
-
-            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                         % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
-
-    # Save checkpoint.
-    acc = 100.*correct/total
-    if acc > best_acc:
-        print('Saving..')
-        state = {
-            'net': net.state_dict(),
-            'acc': acc,
-            'epoch': epoch,
-        }
-        if not os.path.isdir('checkpoint'):
-            os.mkdir('checkpoint')
-        torch.save(state, './checkpoint/ckpt.pth')
-        best_acc = acc
+miss_index, hit_index = get_idxs(test_predictions, test_targets, device)
 
 
-for epoch in range(start_epoch, start_epoch+200):
-    train(epoch)
-    test(epoch)
-    scheduler.step()
+print(f"----------missclassifid index length is {len(miss_index)}----------")
+print(
+    f"----------Correctly classified index length is {len(hit_index)}----------")
+
+
+print(f"----------Visualize model predictions----------")
+show_images_pred(test_images, test_targets, test_predictions, denorm)
+
+
+print(f"----------Visualize misclassified predictions----------")
+show_images_pred(test_images[miss_index], test_targets[miss_index],
+                 test_predictions[miss_index], denorm)
+
+
+print(f"----------Generate heatmaps for test images----------")
+heatmaps = gradcam_heatmap(model, test_predictions, test_images, device)
+
+
+hit_maps = heatmaps[hit_index]
+miss_maps = heatmaps[miss_index]
+
+print(f"----------isualize misclassified GRADCAM----------")
+show_images_cam(test_images, test_targets, test_predictions,
+                heatmaps, miss_index, denorm)
+
+
+print(f"----------Visualize correctly classified GRADCAM----------")
+show_images_cam(test_images, test_targets, test_predictions,
+                heatmaps, hit_index, denorm)
--- utils.py
+++ utils.py
@@ -1,124 +1,406 @@
-'''Some helper functions for PyTorch, including:
-    - get_mean_and_std: calculate the mean and std value of dataset.
-    - msr_init: net parameter initialization.
-    - progress_bar: progress bar mimic xlua.progress.
-'''
-import os
-import sys
-import time
-import math
+import torch
+import torchvision
+from torchsummary import summary
 
-import torch.nn as nn
-import torch.nn.init as init
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
 
-def get_mean_and_std(dataset):
-    '''Compute the mean and std value of dataset.'''
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=2)
-    mean = torch.zeros(3)
-    std = torch.zeros(3)
-    print('==> Computing mean and std..')
-    for inputs, targets in dataloader:
-        for i in range(3):
-            mean[i] += inputs[:,i,:,:].mean()
-            std[i] += inputs[:,i,:,:].std()
-    mean.div_(len(dataset))
-    std.div_(len(dataset))
-    return mean, std
-
-def init_params(net):
-    '''Init layer parameters.'''
-    for m in net.modules():
-        if isinstance(m, nn.Conv2d):
-            init.kaiming_normal(m.weight, mode='fan_out')
-            if m.bias:
-                init.constant(m.bias, 0)
-        elif isinstance(m, nn.BatchNorm2d):
-            init.constant(m.weight, 1)
-            init.constant(m.bias, 0)
-        elif isinstance(m, nn.Linear):
-            init.normal(m.weight, std=1e-3)
-            if m.bias:
-                init.constant(m.bias, 0)
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import cv2
 
 
-_, term_width = os.popen('stty size', 'r').read().split()
-term_width = int(term_width)
+from torch.optim.lr_scheduler import OneCycleLR
+import torch.optim as optim
 
-TOTAL_BAR_LENGTH = 65.
-last_time = time.time()
-begin_time = last_time
-def progress_bar(current, total, msg=None):
-    global last_time, begin_time
-    if current == 0:
-        begin_time = time.time()  # Reset for new bar.
+import numpy as np
 
-    cur_len = int(TOTAL_BAR_LENGTH*current/total)
-    rest_len = int(TOTAL_BAR_LENGTH - cur_len) - 1
+train_losses = []
+test_losses = []
+train_acc = []
+test_acc = []
 
-    sys.stdout.write(' [')
-    for i in range(cur_len):
-        sys.stdout.write('=')
-    sys.stdout.write('>')
-    for i in range(rest_len):
-        sys.stdout.write('.')
-    sys.stdout.write(']')
 
-    cur_time = time.time()
-    step_time = cur_time - last_time
-    last_time = cur_time
-    tot_time = cur_time - begin_time
 
-    L = []
-    L.append('  Step: %s' % format_time(step_time))
-    L.append(' | Tot: %s' % format_time(tot_time))
-    if msg:
-        L.append(' | ' + msg)
+classes = ["airplane", "automobile", "bird", "cat", "deer", 
+           "dog", "frog", "horse", "ship", "truck"]
 
-    msg = ''.join(L)
-    sys.stdout.write(msg)
-    for i in range(term_width-int(TOTAL_BAR_LENGTH)-len(msg)-3):
-        sys.stdout.write(' ')
 
-    # Go back to the center of the bar.
-    for i in range(term_width-int(TOTAL_BAR_LENGTH/2)+2):
-        sys.stdout.write('\b')
-    sys.stdout.write(' %d/%d ' % (current+1, total))
 
-    if current < total-1:
-        sys.stdout.write('\r')
-    else:
-        sys.stdout.write('\n')
-    sys.stdout.flush()
+class Cifar10SearchDataset(torchvision.datasets.CIFAR10):
+    def __init__(self, root="~/data/cifar10", train=True, download=True, transform=None):
+        super().__init__(root=root, train=train, download=download, transform=transform)
 
-def format_time(seconds):
-    days = int(seconds / 3600/24)
-    seconds = seconds - days*3600*24
-    hours = int(seconds / 3600)
-    seconds = seconds - hours*3600
-    minutes = int(seconds / 60)
-    seconds = seconds - minutes*60
-    secondsf = int(seconds)
-    seconds = seconds - secondsf
-    millis = int(seconds*1000)
+    def __getitem__(self, index):
+        image, label = self.data[index], self.targets[index]
 
-    f = ''
-    i = 1
-    if days > 0:
-        f += str(days) + 'D'
-        i += 1
-    if hours > 0 and i <= 2:
-        f += str(hours) + 'h'
-        i += 1
-    if minutes > 0 and i <= 2:
-        f += str(minutes) + 'm'
-        i += 1
-    if secondsf > 0 and i <= 2:
-        f += str(secondsf) + 's'
-        i += 1
-    if millis > 0 and i <= 2:
-        f += str(millis) + 'ms'
-        i += 1
-    if f == '':
-        f = '0ms'
-    return f
+        if self.transform is not None:
+            transformed = self.transform(image=image)
+            image = transformed["image"]
+
+        return image, label
+
+
+
+class UnNormalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        """
+        Args:
+            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+        Returns:
+            Tensor: Normalized image.
+        """
+        for t, m, s in zip(tensor, self.mean, self.std):
+            t.mul_(s).add_(m)
+            # The normalize code -> t.sub_(m).div_(s)
+        return tensor
+
+
+def get_train_transform(MEAN, STD, PAD=4):
+
+    train_transform = A.Compose([
+                                A.PadIfNeeded(min_height=32+PAD, 
+                                            min_width=32+PAD, 
+                                            border_mode=cv2.BORDER_CONSTANT,
+                                            value=(MEAN)),
+                                A.RandomCrop(32, 32),
+                                A.Cutout(max_h_size=16, max_w_size=16),
+                                A.Normalize(mean=(MEAN), 
+                                            std=STD),
+                                ToTensorV2(),
+    ])
+    return(train_transform)
+
+
+def get_test_transform(MEAN, STD):
+
+    test_transform = A.Compose([
+                                A.Normalize(mean=MEAN, 
+                                            std=STD),
+                                ToTensorV2(),
+    ])
+    return(test_transform)
+
+
+
+
+def get_summary(model, device):
+  """
+  Args:
+      model (torch.nn Model): Original data with no preprocessing
+      device (str): cuda/CPU
+  """
+  print(summary(model, input_size=(3, 32, 32)))
+
+
+def get_stats(trainloader):
+  """
+  Args:
+      trainloader (trainloader): Original data with no preprocessing
+  Returns:
+      mean: per channel mean
+      std: per channel std
+  """
+  train_data = trainloader.dataset.data
+
+  print('[Train]')
+  print(' - Numpy Shape:', train_data.shape)
+  print(' - Tensor Shape:', train_data.shape)
+  print(' - min:', np.min(train_data))
+  print(' - max:', np.max(train_data))
+
+  train_data = train_data / 255.0
+
+  mean = np.mean(train_data, axis=tuple(range(train_data.ndim-1)))
+  std = np.std(train_data, axis=tuple(range(train_data.ndim-1)))
+
+  print(f'\nDataset Mean - {mean}')
+  print(f'Dataset Std - {std} ')
+
+  return([mean, std])
+
+
+def get_train_loader(transform=None):
+  """
+  Args:
+      transform (transform): Albumentations transform
+  Returns:
+      trainloader: DataLoader Object
+  """
+  if transform:
+    trainset = Cifar10SearchDataset(transform=transform)
+  else:
+    trainset = Cifar10SearchDataset(root="~/data/cifar10", train=True, 
+                                    download=True)
+  trainloader = torch.utils.data.DataLoader(trainset, batch_size=128,
+                                            shuffle=True, num_workers=2)
+  return(trainloader)
+
+
+def get_test_loader(transform=None):
+  """
+  Args:
+      transform (transform): Albumentations transform
+  Returns:
+      testloader: DataLoader Object
+  """
+  if transform:
+    testset = Cifar10SearchDataset(transform=transform, train=False)
+  else:
+    testset = Cifar10SearchDataset(train=False)
+  testloader = torch.utils.data.DataLoader(testset, batch_size=128, 
+                                         shuffle=False, num_workers=2)
+
+  return(testloader)
+
+
+def get_device():
+  """
+  Returns:
+      device (str): device type
+  """
+  SEED = 1
+
+  # CUDA?
+  cuda = torch.cuda.is_available()
+  print("CUDA Available?", cuda)
+
+  use_cuda = torch.cuda.is_available()
+  device = torch.device("cuda" if use_cuda else "cpu")
+
+  # For reproducibility
+  if cuda:
+      torch.cuda.manual_seed(SEED)
+  else:
+    torch.manual_seed(SEED)
+
+  return(device)
+
+
+def superimpose(heatmap, img, denorm):
+  img = np.transpose(denorm(img.cpu()), (1, 2, 0))
+  heatmap = cv2.resize(heatmap.numpy(), (img.shape[1], img.shape[0]))
+  heatmap = np.uint8(255*heatmap)
+  heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+  superimposed_img = (heatmap * 0.4) + 255*img.numpy()
+  return((heatmap, superimposed_img/superimposed_img.max()))
+
+
+def train(model, device, criterion, train_loader, optimizer, epoch):
+  model.train()
+  pbar = tqdm(train_loader)
+  correct = 0
+  processed = 0
+  for batch_idx, (data, target) in enumerate(pbar):
+    # get samples
+    data, target = data.to(device), target.to(device)
+
+    # Init
+    optimizer.zero_grad()
+    # In PyTorch, we need to set the gradients to zero before starting to do backpropragation because PyTorch accumulates the gradients on subsequent backward passes. 
+    # Because of this, when you start your training loop, ideally you should zero out the gradients so that you do the parameter update correctly.
+
+    # Predict
+    y_pred = model(data)
+
+    # Calculate loss
+    loss = criterion(y_pred, target)
+    train_losses.append(loss)
+
+    # Backpropagation
+    loss.backward()
+    optimizer.step()
+
+    # Update pbar-tqdm
+    
+    pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+    correct += pred.eq(target.view_as(pred)).sum().item()
+    processed += len(data)
+
+    pbar.set_description(desc= f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+    train_acc.append(100*correct/processed)
+
+def test(model, device, criterion, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += criterion(output, target).item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    test_losses.append(test_loss)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    
+    test_acc.append(100. * correct / len(test_loader.dataset))
+
+
+
+def train_model(model, criterion, device, train_loader, test_loader, optimizer, scheduler, EPOCHS):
+  """
+  Args:
+      model (torch.nn Model): Original data with no preprocessing
+      criterion (criterion) - Loss Function
+      device (str): cuda/CPU
+      train_loader (DataLoader) - DataLoader Object
+      optimizer (optimizer) - Optimizer Object
+      scheduler (scheduler) - scheduler object
+      EPOCHS (int) - Number of epochs
+  Returns:
+      results (list): Train/test - Accuracy/Loss 
+  """
+  for epoch in range(EPOCHS):
+      print("EPOCH:", epoch)
+      train(model, device, criterion, train_loader, optimizer, epoch)
+      scheduler.step()
+      test(model, device, criterion, test_loader)
+
+  results = [train_losses, test_losses, train_acc, test_acc]
+  return(results)
+
+
+
+def get_idxs(results, test_targets, device):
+  """
+  Args:
+      results (tensor): predictions
+      test_targets (tensor): Ground truth labels
+  Returns:
+      miss_index: index of misclassifier images
+      hit_index: index of correctly classifier images
+  """
+  miss_index = torch.where((results.argmax(dim=1) == torch.tensor(test_targets).to(device)) == False)[0]
+  hit_index = torch.where((results.argmax(dim=1) == torch.tensor(test_targets).to(device)) == True)[0]
+  return((miss_index, hit_index))
+
+
+def show_images_pred(images, targets, preds, denorm):
+  """
+  Args:
+      images (tensor): images array
+      targets (tensor): Ground truth labels
+      preds (tensor): Predictions
+  """
+  plt.figure(figsize=(15 ,15))
+  for i in range(16):
+      plt.subplot(4, 4,i+1)
+      label = classes[targets[i].cpu()]
+      pred = classes[preds[i].cpu().argmax()]
+      plt.title(f"(T)-{label} - (P)-{pred}")
+      plt.imshow(np.transpose(denorm(images[i].cpu()), (1, 2, 0)))
+  plt.show()
+
+
+def gradcam_heatmap(model, results, test_images, device):
+  """
+  Args:
+      model (torch.nn): Torch model
+      test_targets (tensor): Ground truth labels
+      test_images (tensor): images array
+      device (str): Device type
+  Returns:
+      heatmaps (tensor): heatmaps array
+  """
+  results[torch.arange(len(results)), 
+          results.argmax(dim=1)].backward(torch.ones_like(results.argmax(dim=1)))
+
+  gradients = model.get_activations_gradient()
+
+  pooled_gradients = torch.mean(gradients, dim=[2, 3])
+
+
+  activations = model.get_activations(test_images.to(device)).detach()
+
+  # weight the channels by corresponding gradients
+  for j in range(activations.shape[0]):
+    for i in range(512):
+        activations[j, i, :, :] *= pooled_gradients[j, i]
+      
+
+  # average the channels of the activations
+  heatmaps = torch.mean(activations, dim=1).squeeze()
+
+
+  # relu on top of the heatmap
+  heatmaps = np.maximum(heatmaps.cpu(), 0)
+
+  # normalize the heatmap
+  heatmaps /= torch.max(heatmaps)
+
+  return(heatmaps)
+
+
+def superimpose(heatmap, img, denorm):
+  """
+  Args:
+      heatmap (tensor): Gradient heatmap
+      img (tensor): Image array
+  Returns:
+      superimposed_img (numpy): image array
+  """
+  img = np.transpose(denorm(img.cpu()), (1, 2, 0))
+  heatmap = cv2.resize(heatmap.numpy(), (img.shape[1], img.shape[0]))
+  heatmap = np.uint8(255*heatmap)
+  heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+  superimposed_img = (heatmap * 0.4) + 255*img.numpy()
+  
+  return(superimposed_img/superimposed_img.max())
+
+def show_images_cam(images, targets, preds, heatmaps, idx, denorm):
+  """
+  Args:
+      images (tensor): Images array
+      targets (tensor): Ground truth labels
+      preds (tensor): Predictions
+      heatmaps (tensor): Gradient heatmaps
+      idx (tensor): Subset index
+  Returns:
+      heatmaps (tensor): heatmaps array
+  """
+  images, targets = images[idx], targets[idx]
+  preds, heatmaps = preds[idx], heatmaps[idx]
+
+  plt.figure(figsize=(15 ,15))
+  for i in range(16):
+      plt.subplot(4, 4,i+1)
+      label = classes[targets[i].cpu()]
+      pred = classes[preds[i].cpu().argmax()]
+      plt.title(f"(T)-{label} - (P)-{pred}")
+      plt.imshow(superimpose(heatmaps[i], images[i], denorm))
+      
+  plt.show()
+
+
+def make_plot(results):
+    """
+    Args:
+        images (list of list): Loss & Accuracy List
+    """
+    tr_losses = results[0]
+    te_losses = results[1]
+    tr_acc = results[2]
+    te_acc = results[3]
+
+
+    fig, axs = plt.subplots(2,2,figsize=(15,10))
+    axs[0, 0].plot(tr_losses)
+    axs[0, 0].set_title("Training Loss")
+    axs[1, 0].plot(tr_acc)
+    axs[1, 0].set_title("Training Accuracy")
+    axs[0, 1].plot(te_losses)
+    axs[0, 1].set_title("Test Loss")
+    axs[1, 1].plot(te_acc)
+    axs[1, 1].set_title("Test Accuracy")
+    plt.show()
